591a13b80d15c647b23e386e629c0f73c924a81d
PROTOTYPE8768 <--global>
2024-11-21 20:47:28 +0800

    es

--- dist/js/torres--megacars--script.js
+++ dist/js/torres--megacars--script.js
@@ -2,10 +2,12 @@ const burger = document.querySelector(".header--menu--button");
 const green = document.querySelector(".green-background");
 const scr = document.querySelector("body")
 const burgerButtons = document.querySelectorAll(".burger-button")
+const logs =document.querySelector(".header--logo");
 burger.addEventListener("click", () => {
     burger.classList.toggle("open");
     green.classList.toggle("open");
     scr.classList.toggle("open");
+    logs.classList.toggle("open");
     burgerButtons[0].classList.toggle("open"); 
     burgerButtons[1].classList.toggle("open"); 
 });
