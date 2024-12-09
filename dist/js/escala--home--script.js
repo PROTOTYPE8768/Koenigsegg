283e2608a688405245d7c253828da339751cc18d
PROTOTYPE8768 <--global>
2024-12-09 20:10:54 +0800

    ilsd

--- dist/js/escala--home--script.js
+++ dist/js/escala--home--script.js
@@ -54,40 +54,37 @@ window.addEventListener('scroll', () => {
 
 
 document.querySelectorAll('.tab--button').forEach((tab, index) => {
-    tab.addEventListener('click', function() {
+    tab.addEventListener('click', function () {
         
         document.querySelectorAll('.tab--button').forEach(t => t.classList.remove('active'));
         tab.classList.add('active');
-        
+
         
         const underline = document.querySelector('.underline');
-        underline.style.transform = `translateX(${13 * index}rem)`; 
+        const screenWidth = window.innerWidth;
+
+        if (screenWidth <= 390) {
+            underline.style.transform = `translateX(${8 * index}rem)`; 
+        }
         
+        else {
+            underline.style.transform = `translateX(${13 * index}rem)`; 
+        }
+
         
         const newTitle = tab.getAttribute('data-title');
         const titleElement = document.querySelector('.secondpage-tab-title .tab-title');
-        titleElement.style.opacity = 0; 
+        titleElement.style.opacity = 0;
         setTimeout(() => {
             titleElement.textContent = newTitle;
-            titleElement.style.opacity = 1; 
+            titleElement.style.opacity = 1;
         }, 300);
-        const newTitlee = tab.getAttribute('datatitle');
-        const titleElementt = document.querySelector('.secondpage-tab-title .tabtitle');
-        titleElementt.style.opacity = 0; 
-        setTimeout(() => {
-            titleElementt.textContent = newTitlee;
-            titleElementt.style.opacity = 1; 
-        }, 300);
+
         
-       
         const newBg = tab.getAttribute('data-bg');
         document.querySelector('.escala--secondpage').style.backgroundImage = `url(${newBg})`;
-        newBg.style.opacity = 0; 
-        setTimeout(() => {
-            newBg.textContent = newBg;
-            newBg.style.opacity = 1; 
-        }, 300);
     });
 });
+
 
 
